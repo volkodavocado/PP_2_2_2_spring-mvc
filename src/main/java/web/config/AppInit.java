7405f6cbc08492e6d988c32931defb574ad9176c
volkodavocado <olga.braulova@gmail.com>
2023-12-08 14:58:03 +0300

    add car view

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -7,9 +7,8 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     // Метод, указывающий на класс конфигурации
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return null;
+        return new Class<?>[]{WebConfig.class};
     }
-
 
     // Добавление конфигурации, в которой инициализируем ViewResolver, для корректного отображения jsp.
     @Override
@@ -18,7 +17,6 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
                 WebConfig.class
         };
     }
-
 
     /* Данный метод указывает url, на котором будет базироваться приложение */
     @Override
